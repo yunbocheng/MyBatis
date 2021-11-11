c1db3d7cf6f443be5707c27afeb40691a46e8f6a
YunboCheng <2484720940@qq.com>
2021-11-11 17:35:07 +0800

    ✨补充功能

--- PageHelper分页/src/test/java/org/example/Test.java
+++ PageHelper分页/src/test/java/org/example/Test.java
@@ -15,6 +15,10 @@ public class Test {
     StudentDao dao = sqlSession.getMapper(StudentDao.class);
 
     // 测试分页查询
+    /*
+        当测试类的类名与junit注解名字一致的时候，会使用junit的全限定名称
+        解决办法：修改测试类的类名即可。
+    */
     @org.junit.Test
     public void testHelper(){
         // 加入PageHelper的方法，分页
